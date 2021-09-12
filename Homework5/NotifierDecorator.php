fd79a75a0f712850944ae41b6517d90f3c467ce9
eaglecrazy <munoyoo3munoyoo3>
2021-09-12 13:52:36 +0300

    Реализовал декоратор.

---
+++ Homework5/NotifierDecorator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Homework5;
+
+abstract class NotifierDecorator implements Notifier
+{
+    // В методичке описан пример декоратора который обязательно имеет реализацию по умолчанию.
+    // Это не гибкое решение, так как реализацией по умолчанию, допустим, может быть Email.
+    // А уведомление, допустим, должно уйти только на Slack и SMS.
+    // В моей реализации можно выбрать любое из семи сочетаний Email, Slack и SMS.
+
+    private ?Notifier $notifier;
+
+    /**
+     * @param Notifier|null $notifier
+     */
+    public function __construct(Notifier $notifier = null)
+    {
+        $this->notifier = $notifier;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function notify(string $notification, array $recipients = []): void
+    {
+        if ($this->notifier) {
+            $this->notifier->notify($notification, $recipients);
+        }
+    }
+}
---
+++ homework5-decorator.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Реализовать на PHP пример Декоратора, позволяющий отправлять уведомления несколькими различными способами.
+ */
+require 'vendor/autoload.php';
+
+use Homework5\Notifier\Email;
+use Homework5\Notifier\Slack;
+use Homework5\Notifier\Sms;
+
+$separator = '***********************************' . PHP_EOL;
+
+$message = 'Hello world!';
+$recipients = [
+    Email::class => ['test@yandex.ru', 'test@mail.ru'],
+    Sms::class   => ['+79211234567', '+79117654321'],
+    Slack::class => ['slack-account-1', 'slack-account-2'],
+];
+
+echo $separator;
+$agile_notifier = new Email();
+$agile_notifier->notify($message, $recipients);
+echo $separator;
+$agile_notifier = new Sms($agile_notifier);
+$agile_notifier->notify($message, $recipients);
+echo $separator;
+$agile_notifier = new Slack($agile_notifier);
+$agile_notifier->notify($message, $recipients);
